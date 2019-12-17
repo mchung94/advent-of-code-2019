af0a854e058e9dd531692b020415b3a7257b7b76
Mitchell Chung <mchung94@gmail.com>
2019-12-16 20:06:03 -0800

    Add day 10 part 2

--- day10.lisp
+++ day10.lisp
@@ -5,8 +5,10 @@
           while line
           collect line)))
 
-(deftype asteroid ()
-  "Asteroids are represented as conses containing (x . y) coordinates."
+(deftype coordinates ()
+  "Coordinates are represented as conses containing (x . y) values.
+Points, asteroids, and the location of the monitoring station are represented
+by their coordinates."
   '(cons integer integer))
 
 (defun make-asteroids (&optional (lines (read-lines)))
@@ -25,15 +27,21 @@
         do (setf (gethash asteroid mapping) t)
         finally (return mapping)))
 
-(defun direction (asteroid1 asteroid2)
+(deftype direction ()
+  "Directions are represented by conses containing (dx . dy) values."
+  ;; This happens to have the same definition as coordinates but they aren't
+  ;; related types.
+  '(cons integer integer))
+
+(defun direction (coordinates1 coordinates2)
   "Return a (dx . dy) cons containing the simplified difference in x and y
-coordinates between the two asteroids. Vertical and horizontal directions will
+values between the two coordinates. Vertical and horizontal directions will
 be represented with conses like (-1 . 0), (1 . 0), (0 . -1), or (0 . 1).
-Repeatedly adding the dx and dy values to asteroid1's x and y will eventually
-lead to asteroid2. This is different than the slope, since it takes into
-account increasing or decreasing coordinates."
-  (let ((dx (- (car asteroid2) (car asteroid1)))
-        (dy (- (cdr asteroid2) (cdr asteroid1))))
+Repeatedly adding the dx and dy values to coordinates1's x and y will
+eventually lead to coordinates2. This is different than the slope, since it
+takes into account increasing or decreasing coordinates."
+  (let ((dx (- (car coordinates2) (car coordinates1)))
+        (dy (- (cdr coordinates2) (cdr coordinates1))))
     (cond ((zerop dx) (cons 0 (signum dy)))
           ((zerop dy) (cons (signum dx) 0))
           (t (let ((gcd (gcd dx dy)))
@@ -83,78 +91,147 @@ There must be at least one asteroid in the list."
                   collect (cons asteroid (detectable-asteroids asteroid ht)))
             (lambda (cons) (length (cdr cons)))))
 
+
+(defvar *example1* (make-asteroids (list ".#..#"
+                                         "....."
+                                         "#####"
+                                         "....#"
+                                         "...##")))
+
+(defvar *example2* (make-asteroids (list "......#.#."
+                                         "#..#.#...."
+                                         "..#######."
+                                         ".#.#.###.."
+                                         ".#..#....."
+                                         "..#....#.#"
+                                         "#..#....#."
+                                         ".##.#..###"
+                                         "##...#..#."
+                                         ".#....####")))
+
+(defvar *example3* (make-asteroids (list "#.#...#.#."
+                                         ".###....#."
+                                         ".#....#..."
+                                         "##.#.#.#.#"
+                                         "....#.#.#."
+                                         ".##..###.#"
+                                         "..#...##.."
+                                         "..##....##"
+                                         "......#..."
+                                         ".####.###.")))
+
+(defvar *example4* (make-asteroids (list ".#..#..###"
+                                         "####.###.#"
+                                         "....###.#."
+                                         "..###.##.#"
+                                         "##.##.#.#."
+                                         "....###..#"
+                                         "..#.#..#.#"
+                                         "#..#.#.###"
+                                         ".##...##.#"
+                                         ".....#.#..")))
+
+(defvar *example5* (make-asteroids (list ".#..##.###...#######"
+                                         "##.############..##."
+                                         ".#.######.########.#"
+                                         ".###.#######.####.#."
+                                         "#####.##.#.##.###.##"
+                                         "..#####..#.#########"
+                                         "####################"
+                                         "#.####....###.#.#.##"
+                                         "##.#################"
+                                         "#####.##.###..####.."
+                                         "..######..##.#######"
+                                         "####.##.####...##..#"
+                                         ".#####..#.######.###"
+                                         "##...#.##########..."
+                                         "#.##########.#######"
+                                         ".####.#.###.###.#.##"
+                                         "....##.##.###..#####"
+                                         ".#.#.###########.###"
+                                         "#.#.#.#####.####.###"
+                                         "###.##.####.##.#..##")))
+
+
 (defun test-part1-answer (best-asteroid expected-asteroid expected-detectable-size)
   (assert (equal expected-asteroid (car best-asteroid)))
   (assert (= expected-detectable-size (length (cdr best-asteroid)))))
 
 (defun test-part1 ()
   "Test the examples in the problem description for part 1."
-  (test-part1-answer
-   (best-asteroid (make-asteroids (list ".#..#"
-                                        "....."
-                                        "#####"
-                                        "....#"
-                                        "...##")))
-   (cons 3 4) 8)
-  (test-part1-answer
-   (best-asteroid (make-asteroids (list "......#.#."
-                                        "#..#.#...."
-                                        "..#######."
-                                        ".#.#.###.."
-                                        ".#..#....."
-                                        "..#....#.#"
-                                        "#..#....#."
-                                        ".##.#..###"
-                                        "##...#..#."
-                                        ".#....####")))
-   (cons 5 8) 33)
-  (test-part1-answer
-   (best-asteroid (make-asteroids (list "#.#...#.#."
-                                        ".###....#."
-                                        ".#....#..."
-                                        "##.#.#.#.#"
-                                        "....#.#.#."
-                                        ".##..###.#"
-                                        "..#...##.."
-                                        "..##....##"
-                                        "......#..."
-                                        ".####.###.")))
-   (cons 1 2) 35)
-  (test-part1-answer
-   (best-asteroid (make-asteroids (list ".#..#..###"
-                                        "####.###.#"
-                                        "....###.#."
-                                        "..###.##.#"
-                                        "##.##.#.#."
-                                        "....###..#"
-                                        "..#.#..#.#"
-                                        "#..#.#.###"
-                                        ".##...##.#"
-                                        ".....#.#..")))
-   (cons 6 3) 41)
-  (test-part1-answer
-   (best-asteroid (make-asteroids (list ".#..##.###...#######"
-                                        "##.############..##."
-                                        ".#.######.########.#"
-                                        ".###.#######.####.#."
-                                        "#####.##.#.##.###.##"
-                                        "..#####..#.#########"
-                                        "####################"
-                                        "#.####....###.#.#.##"
-                                        "##.#################"
-                                        "#####.##.###..####.."
-                                        "..######..##.#######"
-                                        "####.##.####...##..#"
-                                        ".#####..#.######.###"
-                                        "##...#.##########..."
-                                        "#.##########.#######"
-                                        ".####.#.###.###.#.##"
-                                        "....##.##.###..#####"
-                                        ".#.#.###########.###"
-                                        "#.#.#.#####.####.###"
-                                        "###.##.####.##.#..##")))
-   (cons 11 13) 210))
+  (test-part1-answer (best-asteroid *example1*) (cons 3 4) 8)
+  (test-part1-answer (best-asteroid *example2*) (cons 5 8) 33)
+  (test-part1-answer (best-asteroid *example3*) (cons 1 2) 35)
+  (test-part1-answer (best-asteroid *example4*) (cons 6 3) 41)
+  (test-part1-answer (best-asteroid *example5*) (cons 11 13) 210))
 
 (defun part1 ()
   "Return the solution to part 1."
   (length (cdr (best-asteroid (make-asteroids)))))
+
+;;;; Part 2
+
+(defun angle (direction)
+  "Return a direction using a double-float number representing degrees,
+where straight up is 0, right is 90, down is 180, left is 270.
+This will break down when the direction values are very large, due to floating
+point calculations."
+  (let* ((x (float (car direction) 0d0))
+         (y (- (float (cdr direction) 0d0)))
+         (radians (atan y x))
+         (degrees (* radians (/ 180d0 pi))))
+    (cond ((<= 0d0 degrees 90d0) (abs (- degrees 90d0)))
+          ((and (< 90d0 degrees) (<= degrees 180d0)) (- 450 degrees))
+          (t (+ (abs degrees) 90d0)))))
+
+(defun angle-to-asteroids-table (asteroids laser-coordinates)
+  "Return a hash table where the key is a direction and the value is a list of
+asteroids in the same direction from the laser."
+  (loop with table = (make-hash-table :test #'equal)
+        for asteroid in asteroids
+        for angle = (angle (direction laser-coordinates asteroid))
+        do (push asteroid (gethash angle table))
+        finally (return table)))
+                                   
+(defun asteroid-vaporized-list (asteroids laser-coordinates)
+  "Return a list of the asteroids in the order they are destroyed by the laser."
+  (let ((table (angle-to-asteroids-table asteroids laser-coordinates)))
+    (labels ((distance (asteroid)
+               "Return the Manhattan distance from the laser to the asteroid."
+               (+ (abs (- (car asteroid) (car laser-coordinates)))
+                  (abs (- (cdr asteroid) (cdr laser-coordinates))))))
+      (let* ((angle-plus-asteroids (loop for angle being the hash-keys of table
+                                         using (hash-value asteroids)
+                                         collect (cons angle (sort asteroids #'< :key #'distance))))
+             (sorted-by-angle (sort angle-plus-asteroids #'< :key #'car))
+             (vector-of-asteroid-lists (map 'vector #'rest sorted-by-angle))
+             (len (length vector-of-asteroid-lists)))
+        (loop until (every #'null vector-of-asteroid-lists)
+              nconc (loop for i from 0 below len
+                          for asteroid = (pop (svref vector-of-asteroid-lists i))
+                          when asteroid
+                          collect it))))))
+
+(defun test-part2 ()
+  "Test the example from the problem description for part 2."
+  (let ((vec (coerce (asteroid-vaporized-list *example5* '(11 . 13)) 'vector)))
+    (assert (equal (svref vec (1- 1)) '(11 . 12)))
+    (assert (equal (svref vec (1- 2)) '(12 . 1)))
+    (assert (equal (svref vec (1- 3)) '(12 . 2)))
+    (assert (equal (svref vec (1- 10)) '(12 . 8)))
+    (assert (equal (svref vec (1- 20)) '(16 . 0)))
+    (assert (equal (svref vec (1- 50)) '(16 . 9)))
+    (assert (equal (svref vec (1- 100)) '(10 . 16)))
+    (assert (equal (svref vec (1- 199)) '(9 . 6)))
+    (assert (equal (svref vec (1- 200)) '(8 . 2)))
+    (assert (equal (svref vec (1- 201)) '(10 . 9)))
+    (assert (equal (svref vec (1- 300)) '(11 . 1)))))
+
+(defun part2 ()
+  "Return the solution for part 2."
+  (destructuring-bind (x . y)
+      (elt (asteroid-vaporized-list (make-asteroids) '(22 . 25)) (1- 200))
+    (+ (* x 100) y)))
+         
+
+    
